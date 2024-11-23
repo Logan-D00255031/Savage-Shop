4bbaae7051c28db35045e70af27cd53f2a7e7661
Logan Rushe <D00255031@student.dkit.ie>
2024-11-23 03:08:29 +0000

    :video_game: Inventory Button now acts as a toggle + Keybind added

---
+++ Assets/Scripts/IButtonToggle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IButtonToggle
+{
+    public void Activate();
+
+    public void Deactivate();
+}
---
+++ Assets/Scripts/InventoryToggle.cs
@@ -0,0 +1,47 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryToggle : MonoBehaviour
+{
+    public PrefabInventoryManager inventoryManager;
+    public GameObject inventoryContainer;
+
+    [ReadOnly, SerializeField]
+    private bool active = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleState();
+        }
+    }
+
+    public void ToggleState()
+    {
+        if (!active)
+        {
+            active = true;
+            Activate();
+        }
+        else
+        {
+            active = false;
+            Deactivate();
+        }
+    }
+
+    public void Activate()
+    {
+        inventoryContainer.SetActive(active);
+        inventoryManager.ListItems();
+    }
+
+    public void Deactivate()
+    {
+        inventoryContainer.SetActive(active);
+        inventoryManager.CleanInventory();
+    }
+}
