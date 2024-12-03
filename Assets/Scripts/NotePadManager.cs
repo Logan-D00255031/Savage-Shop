aa83d999d751e18510b7a8d2ac49d8818d4658a0
TitilayoAkande <D00250764@student.dkit.ie>
2024-12-03 21:14:34 +0000

    👷 Implementes UI + Working Start Game Button and Notepad Button

---
+++ Assets/Scripts/NotePadManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class NotepadManager : MonoBehaviour
+{
+    public GameObject notepadPanel; // Assign your Notepad Panel in the Inspector
+
+    public void ToggleNotepad()
+    {
+        // Toggle the visibility of the Notepad Panel
+        notepadPanel.SetActive(!notepadPanel.activeSelf);
+    }
+}
