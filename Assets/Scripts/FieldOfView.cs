d1cec1f0cd893382bc4ce3e504b7b61ba8b2f048
Logan Rushe <D00255031@student.dkit.ie>
2024-12-13 19:18:32 +0000

    🚧 Starting Enemy AI + Minor scene changes
    Added a field of view script sourced online for enemy
    Moved some decorations to prevent clipping
    Modified the player

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,68 @@
+// Script from tutorial video: https://youtu.be/j1-OyLo77ss?si=9mb9EVDjs-W1uyBo
+
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    [Range(0, 50)]
+    public float radius;
+    [Range(1, 360)]
+    public float angle;
+
+    [Required]
+    public GameObject playerRef;
+
+    [Required]
+    public LayerMask targetMask;
+    [Required]
+    public LayerMask obstructionMask;
+
+    [ReadOnly]
+    public bool canSeePlayer;
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
+    }
+}
